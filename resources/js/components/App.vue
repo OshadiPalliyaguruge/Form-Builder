8f357724ad0748b268e3f88ef8197c5b725a63c9
Oshadi Palliyaguruge <oshadi.palliyaguruge@gmail.com>
2025-09-19 12:03:12 +0530

    Create dashboard UI

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,20 +1,53 @@
 <template>
-  <div>
+  <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">Form Builder</a>
+        <a class="navbar-brand" href="#" @click.prevent="goToHome">Form Builder</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Dashboard</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/forms" class="nav-link">Forms</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/create" class="nav-link">Create Form</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
     
     <div class="container mt-4">
-      <h1>Form Builder is Working!</h1>
-      <p>Vue.js is working with Laravel.</p>
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    goToHome() {
+      this.$router.push('/');
+    }
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.navbar-brand {
+  cursor: pointer;
+}
+</style>
---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Form Builder</h1>
+    <p class="lead">Manage your forms</p>
+    
+    <div class="row mt-5">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">Forms</h5>
+            <p class="card-text">Create and manage your forms</p>
+            <router-link to="/forms" class="btn btn-primary">Manage Forms</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">Create New</h5>
+            <p class="card-text">Build a new form from scratch</p>
+            <router-link to="/create" class="btn btn-success">Create Form</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body text-center">
+            <h5 class="card-title">Submissions</h5>
+            <p class="card-text">View form submissions</p>
+            <button class="btn btn-info" disabled>View Submissions</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
