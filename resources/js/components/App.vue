9faf0709c3208f8e16aaaf0145dc63206afcd4c3
Oshadi Palliyaguruge <oshadi.palliyaguruge@gmail.com>
2025-09-19 11:25:28 +0530

    Add testing code for UI

---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">Form Builder</a>
+      </div>
+    </nav>
+    
+    <div class="container mt-4">
+      <h1>Form Builder is Working!</h1>
+      <p>Vue.js is working with Laravel.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
